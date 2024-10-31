948bed61faa51e9ade7f907d2be6dcf431b5f7bb
ZanoniCFernandes <zanoni.c.fernandes@gmail.com>
2024-10-31 17:05:37 -0300

    feat!: first commit to lab github

---
+++ docker-composer-app/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const port = 3000;
+
+app.get("/api", (req, res) => {
+    res.send(`Olá para Node.js API!`);
+})
+
+app.listen(port, () => {
+    console.log(`Servidor executando na porta ${port}`);
+})
